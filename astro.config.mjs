ac869d6cfcf8b8d96597a03e4646fc3091b83648
valtronic <valentin.yeo@gmail.com>
2026-08-23 03:01:42 +0100

    docs: update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -56,6 +56,7 @@ export default defineConfig({
           'features/observability',
           'features/slack',
           'features/agents',
+          'features/desktop',
         ],
         },
         {
